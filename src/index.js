dd0ccea290d6fd10cffffa34b1c17149c20b3953
katiakniazhevich <55781001+katiakniazhevich@users.noreply.github.com>
2021-03-15 21:10:03 +0300

    feat: added decision

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return ((array == undefined) || (array.length == 0)) ? 0 : Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return ((array == undefined) || (array.length == 0)) ? 0 : Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if ((array == undefined) || (array.length == 0)) return 0;
+    let allNmuber = array.reduce((sum, current) => (sum + current));
+    return allNmuber / array.length;
 }
